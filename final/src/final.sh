5fa42e0f28ec4f91d3912cc1c794ceeeac2b7249
goodbob123 <johnsonsui3@gmail.com>
2023-11-21 23:50:49 +0800

    first version done

--- final/src/final.sh
+++ final/src/final.sh
@@ -1,8 +1,8 @@
 for ((i = 0 ; i < 100 ; i++)); do
     if (($i < 10)); then
-        ./operation.sh all ./benchmarks/ex0${i}.truth ./benchmarks/ex0${i}.truth
+        ./operation.sh all ./benchmarks/ex0${i}.truth ./answer/aig/ex0${i}.aig
     else
-        ./operation.sh all ./benchmarks/ex${i}.truth ./benchmarks/ex${i}.truth
+        ./operation.sh all ./benchmarks/ex${i}.truth ./answer/aig/ex${i}.aig
     fi
 done
 
