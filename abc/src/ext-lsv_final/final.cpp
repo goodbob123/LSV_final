95cc92a9b7275434e37e5e6fd98049a4166b3b04
goodbob123 <johnsonsui3@gmail.com>
2023-12-24 00:27:40 +0800

    SA version1 done

--- abc/src/ext-lsv_final/final.cpp
+++ abc/src/ext-lsv_final/final.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <cmath>
 
 #include "base/abc/abc.h"
 #include "base/main/main.h"
@@ -20,9 +21,11 @@ Aig_Man_t* Abc_NtkToDar(Abc_Ntk_t* pNtk, int fExors, int fRegisters);
 }
 static int Lsv_CommandPrintMessage(Abc_Frame_t* pAbc, int argc, char** argv);
 static int Lsv_CommandBddReorder(Abc_Frame_t* pAbc, int argc, char** argv);
+static int Lsv_CommandSimulatedAnnealing(Abc_Frame_t* pAbc, int argc, char** argv);
 
 void init(Abc_Frame_t* pAbc) {
     Cmd_CommandAdd(pAbc, "LSV", "lsv_print_message", Lsv_CommandPrintMessage, 0);
+    Cmd_CommandAdd(pAbc, "LSV", "lsv_sa", Lsv_CommandSimulatedAnnealing, 0);
     Cmd_CommandAdd(pAbc, "LSV", "lsv_bdd_reorder", Lsv_CommandBddReorder, 0);
 }
 
@@ -44,7 +47,121 @@ int Lsv_CommandPrintMessage(Abc_Frame_t* pAbc, int argc, char** argv) {
 
 // # TODO: some optimize command can be added here (call it after &read , before &write)
 // int Lsv_CommandBobOptimizeSomething(Abc_Frame_t* pAbc, int argc, char** argv) {
+int Lsv_CommandSimulatedAnnealing(Abc_Frame_t* pAbc, int argc, char** argv) {
 
+    Abc_Ntk_t* pNtk;
+    pNtk = Abc_FrameReadNtk(pAbc);
+    srand(time(0));
+
+    // ensure Ntk exist
+    if (pNtk == NULL) {
+        Abc_Print(-1, "Empty network.\n");
+        return 1;
+    }
+    if (!Abc_NtkIsStrash(pNtk)) {
+        Abc_Print(-1, "Simulating BDDs can only be done for BDD networks (run \"collapse\").\n");
+        return 1;
+    }
+
+    //init annealing condition
+    cout<<"Simulated Annealing"<<endl;
+    double start_heat = 1000, end_heat = 0.01, cooling_rate = 0.99;
+
+    //preserve old aig
+    cout<<"Origin and: "<<Abc_NtkNodeNum(pNtk)<<"  "<<endl;
+    double best_eval = Abc_NtkNodeNum(pNtk);
+    char * Command = "write_aiger annealing.aig;";
+    if ( Cmd_CommandExecute(Abc_FrameGetGlobalFrame(), Command) ){
+        Abc_Print( 1, "Something did not work out with the command \"%s\".\n", Command );
+        return 1;
+    }
+
+    for(double heat = start_heat; heat > end_heat; heat *= cooling_rate) {
+        int option = (rand()%7)+1;
+        switch (option){
+            case 1:
+                Command = "rewrite; refactor; resub;";
+                if ( Cmd_CommandExecute(Abc_FrameGetGlobalFrame(), Command) ){
+                    Abc_Print( 1, "Something did not work out with the command \"%s\".\n", Command );
+                    return 1;
+                }
+                break;
+            case 2:
+                Command = "&get; &deepsyn -I 10 -J 50 -T 30 -S 11 -t; &put;";
+                if ( Cmd_CommandExecute(Abc_FrameGetGlobalFrame(), Command) ){
+                    Abc_Print( 1, "Something did not work out with the command \"%s\".\n", Command );
+                    return 1;
+                }
+                break;
+            case 3:
+                Command = "&get; &deepsyn -I 5 -J 50 -T 15 -S 22 -t; &put;";
+                if ( Cmd_CommandExecute(Abc_FrameGetGlobalFrame(), Command) ){
+                    Abc_Print( 1, "Something did not work out with the command \"%s\".\n", Command );
+                    return 1;
+                }
+                break;
+            case 4:
+                Command = "&get; &deepsyn -I 7 -J 50 -T 20 -S 33 -t; &put;";
+                if ( Cmd_CommandExecute(Abc_FrameGetGlobalFrame(), Command) ){
+                    Abc_Print( 1, "Something did not work out with the command \"%s\".\n", Command );
+                    return 1;
+                }
+                break;
+            case 5:
+                Command = "&get; &transduction -T 4 -I 111 -R 117; &dc2; &put;";
+                if ( Cmd_CommandExecute(Abc_FrameGetGlobalFrame(), Command) ){
+                    Abc_Print( 1, "Something did not work out with the command \"%s\".\n", Command );
+                    return 1;
+                }
+                break;
+            case 6:
+                Command = "&get; &transduction -I 123 -R 17; &dc2; &put;";
+                if ( Cmd_CommandExecute(Abc_FrameGetGlobalFrame(), Command) ){
+                    Abc_Print( 1, "Something did not work out with the command \"%s\".\n", Command );
+                    return 1;
+                }
+                break;
+            case 7:
+                Command = "orchestrate;";
+                if ( Cmd_CommandExecute(Abc_FrameGetGlobalFrame(), Command) ){
+                    Abc_Print( 1, "Something did not work out with the command \"%s\".\n", Command );
+                    return 1;
+                }
+                break;
+            default:
+                cout<<"SA_option_error"<<endl;
+                return 1;
+                break;
+        }
+
+        double change = Abc_NtkNodeNum(pNtk) - best_eval;
+        if (change < 0 || (exp((-change-0.1) / heat) > (rand() / (double) RAND_MAX))) {
+            Command = "write_aiger annealing.aig;";
+            if ( Cmd_CommandExecute(Abc_FrameGetGlobalFrame(), Command) ){
+                Abc_Print( 1, "Something did not work out with the command \"%s\".\n", Command );
+                return 1;
+            }
+            best_eval = Abc_NtkNodeNum(pNtk);
+        }
+        else{
+            Command = "read annealing.aig";
+            if ( Cmd_CommandExecute(Abc_FrameGetGlobalFrame(), Command) ){
+                Abc_Print( 1, "Something did not work out with the command \"%s\".\n", Command );
+                return 1;
+            }
+        }
+    }
+
+    Command = "read_aiger annealing.aig";
+    if ( Cmd_CommandExecute(Abc_FrameGetGlobalFrame(), Command) ){
+        Abc_Print( 1, "Something did not work out with the command \"%s\".\n", Command );
+        return 1;
+    }
+
+    remove("annealing.aig");
+    return 0;
+
+}
 
 int Lsv_CommandBddReorder(Abc_Frame_t* pAbc, int argc, char** argv) {
 
--- final/src/eval.sh
+++ final/src/eval.sh
@@ -1,60 +1,64 @@
 command="$1"
-
+rm -rf message
 if [[ $command == "aig" ]]; then
     for ((i = 0 ; i < 100 ; i++)); do
         if (($i < 10)); then
-            if [[ ! -e "./answer/aig/ex0${i}.aig" ]]; then
+            if [[  -e "./answer/aig/ex0${i}.aig" ]]; then
                 filename="./answer/aig/ex0${i}.aig"
-                n=1
-                while read line; do
-                # reading each line
-                echo "Line No. $n : $line"
-                n=$((n+1))
-                done < $filename
+                ./abc -c "read $filename; print_stats;" > message
+                ./eval message
             fi
         else
-            if [[ ! -e "./answer/aig/ex${i}.aig" ]]; then
-                filename='company.txt'
-                n=1
-                while read line; do
-                # reading each line
-                echo "Line No. $n : $line"
-                n=$((n+1))
-                done < $filename
+            if [[  -e "./answer/aig/ex${i}.aig" ]]; then
+                filename="./answer/aig/ex${i}.aig"
+                ./abc -c "read $filename; print_stats;" > message
+                ./eval message
             fi
         fi
     done
 elif [[ $command == "xaig" ]]; then
     for ((i = 0 ; i < 100 ; i++)); do
         if (($i < 10)); then
-            if [[ ! -e "./answer/xaig/ex0${i}.aig" ]]; then
-                
+            if [[  -e "./answer/xaig/ex0${i}.aig" ]]; then
+                filename="./answer/aig/ex0${i}.aig"
+                ./abc -c "read $filename; print_stats;" > message
+                ./eval message
             fi
         else
-            if [[ ! -e "./answer/xaig/ex${i}.aig" ]]; then
-                
+            if [[  -e "./answer/xaig/ex${i}.aig" ]]; then
+                filename="./answer/aig/ex${i}.aig"
+                ./abc -c "read $filename; print_stats;" > message
+                ./eval message
             fi
         fi
     done
 else
     for ((i = 0 ; i < 100 ; i++)); do
         if (($i < 10)); then
-            if [[ ! -e "./answer/aig/ex0${i}.aig" ]]; then
-                
+            if [[  -e "./answer/aig/ex0${i}.aig" ]]; then
+                ffilename="./answer/aig/ex0${i}.aig"
+                ./abc -c "read $filename; print_stats;" > message
+                ./eval message
             fi
-            if [[ ! -e "./answer/xaig/ex0${i}.aig" ]]; then
-                
+            if [[  -e "./answer/xaig/ex0${i}.aig" ]]; then
+                filename="./answer/aig/ex0${i}.aig"
+                ./abc -c "read $filename; print_stats;" > message
+                ./eval message
             fi
         else
-            if [[ ! -e "./answer/aig/ex${i}.aig" ]]; then
-                
+            if [[  -e "./answer/aig/ex${i}.aig" ]]; then
+                filename="./answer/aig/ex${i}.aig"
+                ./abc -c "read $filename; print_stats;" > message
+                ./eval message
             fi
-            if [[ ! -e "./answer/xaig/ex${i}.aig" ]]; then
-                
+            if [[  -e "./answer/xaig/ex${i}.aig" ]]; then
+                filename="./answer/aig/ex${i}.aig"
+                ./abc -c "read $filename; print_stats;" > message
+                ./eval message
             fi
         fi
     done
 fi
-
+rm -rf message
 #rm -rf ./answer/xaig
 #cp -r ./answer/aig ./answer/xaig
